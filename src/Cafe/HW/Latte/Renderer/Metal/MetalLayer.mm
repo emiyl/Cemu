69ce7267ae43f335c794fb6438827082452496f4
emiyl <me@emiyl.com>
2026-05-08 14:49:30 +0100

    increase layer.maximumdrawablecount in metallayer

--- src/Cafe/HW/Latte/Renderer/Metal/MetalLayer.mm
+++ src/Cafe/HW/Latte/Renderer/Metal/MetalLayer.mm
@@ -3,46 +3,41 @@
 #include "Cafe/HW/Latte/Renderer/MetalView.h"
 
 #if TARGET_OS_IOS
-void ConfigureMetalLayerForIOS(void* layerPtr)
-{
-	CAMetalLayer* layer = (CAMetalLayer*)layerPtr;
-	// Disable display-sync so drawables are returned to the pool as soon as the
-	// GPU finishes rather than waiting for the next screen scan-out.  Without
-	// this, the pool (default size 3) exhausts within 3 frames and nextDrawable()
-	// blocks the Latte thread forever, starving the vsync timer.
-	layer.maximumDrawableCount = 2;
+void ConfigureMetalLayerForIOS(void *layerPtr) {
+  CAMetalLayer *layer = (CAMetalLayer *)layerPtr;
+  layer.maximumDrawableCount = 3;
 }
 #endif
 
-void* CreateMetalLayer(void* handle, float& scaleX, float& scaleY)
-{
-	#if TARGET_OS_IOS
-	UIView* view = (UIView*)handle;
+void *CreateMetalLayer(void *handle, float &scaleX, float &scaleY) {
+#if TARGET_OS_IOS
+  UIView *view = (UIView *)handle;
 
-	MetalView* childView = [[MetalView alloc] initWithFrame:view.bounds];
-	childView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  MetalView *childView = [[MetalView alloc] initWithFrame:view.bounds];
+  childView.autoresizingMask =
+      UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
-	[view addSubview:childView];
+  [view addSubview:childView];
 
-	scaleX = childView.contentScaleFactor;
-	scaleY = childView.contentScaleFactor;
+  scaleX = childView.contentScaleFactor;
+  scaleY = childView.contentScaleFactor;
 
-	return childView.layer;
-	#else
-	NSView* view = (NSView*)handle;
+  return childView.layer;
+#else
+  NSView *view = (NSView *)handle;
 
-	MetalView* childView = [[MetalView alloc] initWithFrame:view.bounds];
-	childView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
-	childView.wantsLayer = YES;
+  MetalView *childView = [[MetalView alloc] initWithFrame:view.bounds];
+  childView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+  childView.wantsLayer = YES;
 
-	[view addSubview:childView];
+  [view addSubview:childView];
 
-	const NSRect points = [childView frame];
-    const NSRect pixels = [childView convertRectToBacking:points];
+  const NSRect points = [childView frame];
+  const NSRect pixels = [childView convertRectToBacking:points];
 
-	scaleX = (float)(pixels.size.width / points.size.width);
-    scaleY = (float)(pixels.size.height / points.size.height);
+  scaleX = (float)(pixels.size.width / points.size.width);
+  scaleY = (float)(pixels.size.height / points.size.height);
 
-	return childView.layer;
-	#endif
+  return childView.layer;
+#endif
 }
